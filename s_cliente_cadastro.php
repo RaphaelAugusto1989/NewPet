31aba8c3c2afdaa3f2f3209a6c655a8bee8ecc00
Raphael <raphael.augusto@tivit.com>
2020-02-03 16:50:46 -0300

    alteração na pagina de cadastro de clientes

--- s_cliente_cadastro.php
+++ s_cliente_cadastro.php
@@ -1,7 +1,15 @@
 <?php include "s_header.php";?>
 
 <!-- INICIO DIV CONTEÚDO -->
-<div class="col-3 "></div>
+<div class="col-3">
+    <nav class="col-lg-12 sidebar">
+        <ul class="list-unstyled">
+            <li><a href="#"><i class="fas fa-money-check-alt"></i> Dados Cadastrais</a></li>
+            <li><a href="#"><i class="fas fa-calendar-alt"></i> Endereço</a></li>
+            <li><a href="#"><i class="fas fa-users"></i> Pets</a></li>
+        </ul>
+    </nav>
+</div>
 <div class="col-6">
     <h3 class="text-center">Usuários do Sistema</h3>
     <div class="row">
@@ -9,36 +17,7 @@
             <a href="http://" class="btn btn-success float-right"><i class="fas fa-user-plus"></i> Novo Usuário</a>
         </div>
     </div>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-            <th scope="col">NOME</th>
-            <th scope="col">CPF/CNPJ</th>
-            <th scope="col">EMAIL</th>
-            <th scope="col">TELEFONE</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>Mark</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <td>Mark</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <td>Mark</td>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            </tr>
-        </tbody>
-    </table>
+    
 </div>
 <div class="col-3"></div>
 <!-- FIM DIV CONTEÚDO -->

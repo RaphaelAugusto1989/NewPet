11d32dd67fe90c2b5265c4ed9fc6dd80b361265c
Raphael <raphael.augusto@tivit.com>
2020-02-19 16:55:54 -0300

    alteração do config.php  url amigavel

--- application/config/config.php
+++ application/config/config.php
@@ -43,7 +43,8 @@ $config['base_url'] = 'http://localhost/newpet/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -61,6 +62,7 @@ $config['index_page'] = 'index.php';
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
 $config['uri_protocol']	= 'REQUEST_URI';
+// $config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
